32568e347314cddc7430c5839223a46cc26e380c
Radu Popovici <rpopovici@totalsoft.ro>
2021-01-20 07:35:28 +0200

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Threading.Tasks;
+using NBB.Core.Effects;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static async Task Main()
+        {
+            await using var interpreter = Interpreter.CreateDefault();
+            await interpreter.Interpret(MainEffect);
+        }
+
+        static readonly Effect<Unit> MainEffect = 
+            Effect.From(() => Console.WriteLine("Hello world"));
+    }
+}
